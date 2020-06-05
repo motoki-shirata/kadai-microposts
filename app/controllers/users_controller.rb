8aad7741022a4d4d44a853bfdb513652e20dfb5c
motoki.shirata <shirata.motoki@lmi.ne.jp>
2020-06-05 02:37:25 +0000

    post

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,11 @@ class UsersController < ApplicationController
 		@users = User.order(id: :desc).page(params[:page]).per(25)
 	end
 
-	def show
-		@user = User.find(params[:id])
-	end
+  def show
+    @user = User.find(params[:id])
+    @microposts = @user.microposts.order(id: :desc).page(params[:page])
+    counts(@user)
+  end
 
 	def new
 		@user = User.new
