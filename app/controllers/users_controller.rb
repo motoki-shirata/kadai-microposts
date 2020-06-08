0db8cb85bfd03f70501d86a60c720d16fce60e0e
motoki.shirata <shirata.motoki@lmi.ne.jp>
2020-06-08 07:34:07 +0000

    follow, unfollow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show]
+  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers]
 
-	def index
-		@users = User.order(id: :desc).page(params[:page]).per(25)
-	end
+  def index
+    @users = User.order(id: :desc).page(params[:page]).per(25)
+  end
 
   def show
     @user = User.find(params[:id])
@@ -11,26 +11,37 @@ class UsersController < ApplicationController
     counts(@user)
   end
 
-	def new
-		@user = User.new
-	end
+  def new
+    @user = User.new
+  end
 
-	def create
-		@user = User.new(user_params)
+  def create
+    @user = User.new(user_params)
 
-		if @user.save
-			flash[:success] = 'ユーザを登録しました。'
-			redirect_to @user
-		else
-			flash.now[:danger] = 'ユーザの登録に失敗しました。'
-			render :new
-		end
-	end
+    if @user.save
+      flash[:success] = 'ユーザを登録しました。'
+      redirect_to @user
+    else
+      flash.now[:danger] = 'ユーザの登録に失敗しました。'
+      render :new
+    end
+  end
 
-	private
+  def followings
+    @user = User.find(params[:id])
+    @followings = @user.followings.page(params[:page])
+    counts(@user)
+  end
 
-	def user_params
-		params.require(:user).permit(:name, :email, :password, :password_confirmation)
-	end
+  def followers
+    @user = User.find(params[:id])
+    @followers = @user.followers.page(params[:page])
+    counts(@user)
+  end
 
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
